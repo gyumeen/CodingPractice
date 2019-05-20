3f06a9d7157cad05cab87f6c6ccb35802ed4dc0a
Kyu Min Lee <44955076+gyumeen@users.noreply.github.com>
2019-05-20 17:34:32 +0900

    Update 배열-오류.java

--- 배열-오류.java
+++ 배열-오류.java
@@ -30,3 +30,24 @@ Error:(7, 13) java: ']' expected
 static을  String[] manage 앞에 붙혀 보았지만 이건 메모리 관리를 위한 기능이지
 여기서는 이 문제 해결에는 아무 도움이 되지 못했다.
 */
+
+
+package ManagingProgram;
+
+public class Managing {
+
+    public static void main(String[] args) {
+        String[] manage = new String[3];
+        manage [0]="름";
+        manage [1]="음";
+        manage [2]="앙";
+        System.out.println(manage[0]);
+    }
+}
+
+//출력
+/*
+름
+*/
+
+// 메인 메소드 안에 바로 배열을 인스턴스화해서(?) 만들어야 오류가 나지 않는 건가..
