aab2c5d961f147a6c95b164edd4cfc340ca57289
Kyu Min Lee <44955076+gyumeen@users.noreply.github.com>
2020-02-03 04:00:48 +0900

    Create LeapYearCheck.java

---
+++ LeapYearCheck.java
@@ -0,0 +1,24 @@
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class LeapYearCheck {
+    public static void main(String[] args) {
+        Scanner scannedLeapYearNumber = new Scanner(System.in);
+
+        for(int i = 0; i < 5; i++) {
+            int verifyingLeapYearNumber = scannedLeapYearNumber.nextInt();
+            GregorianCalendar gregorianCalendar_1 = new GregorianCalendar();
+
+            if(gregorianCalendar_1.isLeapYear(verifyingLeapYearNumber)){
+                System.out.println(verifyingLeapYearNumber +" = "+ "윤년");
+            } else System.out.println(verifyingLeapYearNumber +" = "+ "평년");
+
+            /*
+            if(verifyingLeapYearNumber % 4 == 0 && verifyingLeapYearNumber % 100 != 0 || verifyingLeapYearNumber % 400 == 0) {
+                System.out.println(verifyingLeapYearNumber +" = "+ "윤년");
+            } else System.out.println(verifyingLeapYearNumber +" = "+ "평년");
+            */
+        }
+
+    }
+}
