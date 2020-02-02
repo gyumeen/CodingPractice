1a637e031983432c53d99d6cbf72db4300a46c9f
Kyu Min Lee <44955076+gyumeen@users.noreply.github.com>
2020-02-03 04:26:50 +0900

    숫자로 피라미드 모양 만들기 문제
    숫자로 피라미드 모양 만들기 문제

--- NumberPyramid.java
+++ NumberPyramid.java
@@ -10,7 +10,7 @@ public class NumberPyramid {
         
         for(int i = 1; i < inputtedPyramidNumber + 1; i++) {
             for(int j = 1; j < i + 1; j++) {
-                if(j > i){break;}
+                if(j > i) break;
                 System.out.print(j * i + " ");
             }
             System.out.println();
