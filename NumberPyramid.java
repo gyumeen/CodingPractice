3cba0dc96f6366028a5db836791a1bec869f3628
Kyu Min Lee <44955076+gyumeen@users.noreply.github.com>
2020-02-03 03:47:50 +0900

    Update NumberPyramid.java

--- NumberPyramid.java
+++ NumberPyramid.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class Main {
+public class NumberPyramid {
     public static void main(String[] args) {
         
         System.out.println("Input the number which you want to make a pyramid");
         
-        Scanner sc = new Scanner(System.in);
-        int inputedNumber = sc.nextInt();
+        Scanner scannedNumber = new Scanner(System.in);
+        int inputtedNumber = scannedNumber.nextInt();
         
-        for(int i = 1; i < inputedNumber + 1; i++) {
+        for(int i = 1; i < inputtedNumber + 1; i++) {
             for(int j = 1; j < i + 1; j++) {
                 if(j > i){break;}
                 System.out.print(j * i + " ");
